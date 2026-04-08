d020859c131b538dc191492e80446d12850da0ef
merola.riccardo <merola.riccardo02@gmail.com>
2026-04-08 10:37:19 +0200

    Classe App() -semplificata gestione del doppio clic -  modifica header tabella per bug libreria grafica - aggiunta funzione scrittura db Classe DetailWindow() - modifiche a bug grafici Creazione classe DocumentExistAllert()

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import query
 
 ctk.set_appearance_mode("System")   # imposta il tema del sistema
 ctk.set_default_color_theme("blue") # imposta i colori sul blu
+mode = ctk.get_appearance_mode()
 
 # Classe della finestra principale
 class App(ctk.CTk):
@@ -13,6 +14,7 @@ class App(ctk.CTk):
 
         self.title("Archivio Errori")
         self.center_win_app(1500, 830)
+        self.resizable(False, False)
 
         # configurazione griglia principale
         self.grid_columnconfigure(0, weight=0)
@@ -46,7 +48,7 @@ class App(ctk.CTk):
         # Pulsanti per allegare documenti, salvare il db, chiudere il programma
         self.button_allega = ctk.CTkButton(master=self.left_frame, text="Allega documento 📁", font=("Roboto", 15))
         self.button_allega.grid(column=0, row=6, padx=10, pady=10, sticky="ew")
-        self.button_save = ctk.CTkButton(master=self.left_frame, text="Salva in database 💾", font=("Roboto", 15), fg_color="green", hover_color="#218838")
+        self.button_save = ctk.CTkButton(master=self.left_frame, text="Salva in database 💾", font=("Roboto", 15), fg_color="green", hover_color="#218838", command=self.insert_record)
         self.button_save.grid(column=0, row=7, padx=10, pady=10, sticky="ew")
         self.button_exit = ctk.CTkButton(master=self.left_frame, text="Esci dal programma ❌", font=("Roboto", 15), fg_color="red", hover_color="#C82333", command=self.quit)
         self.button_exit.grid(column=0, row=8 , padx=10, pady=10, sticky="sew")
@@ -64,17 +66,22 @@ class App(ctk.CTk):
         # Frame con scroll-bar in cui inserire la tabella
         self.scrollable_frame = ctk.CTkScrollableFrame(self.right_frame, corner_radius=4)
         self.scrollable_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
-        self.scrollable_frame.grid_columnconfigure(0, weight=1)
+        self.scrollable_frame.grid_columnconfigure((0, 1), weight=1)
+        self.scrollable_frame.grid_rowconfigure((0, 1), weight=1)
 
         # Definizione della tabella, della sua header e delle dimensioni delle colonne
-        values = [["ID", "Componente", "Problema", "Soluzione", "Documento"]]
+        headers = "        ID\t          Componente\t\t                 Problema\t\t\t\t                Soluzione\t\t\t         Documento"
+        self.header_table = ctk.CTkLabel(master=self.scrollable_frame, width=1150, corner_radius=4, text=headers, anchor="w",  font=("Roboto", 18, "bold"))
+        self.header_table.grid(row=0, column=0, padx=5, pady=(0, 5), sticky="ew")
+
+        values = []
         self.value_table = CTkTable(master=self.scrollable_frame, row=100, column=5, width=1150, corner_radius=4, values=values, hover=True, command=self.handle_table_click)
         self.value_table.edit_column(0, width=100)
         self.value_table.edit_column(1, width=200)
         self.value_table.edit_column(2, width=350)
         self.value_table.edit_column(3, width=350)
         self.value_table.edit_column(4, width=150)
-        self.value_table.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
+        self.value_table.grid(row=1, column=0, padx=5, pady=(0, 5), sticky="ew")
 
         self.load_data()
 
@@ -110,15 +117,14 @@ class App(ctk.CTk):
         self.geometry(f"{width}x{height}+{x}+{y}")
 
     # Funzione "intelligente" per gestire i click sui record della tabella
-    
     def handle_table_click(self, event):
         row = event["row"]
         # se il click è sulla riga 0 di indice si ignora
-        if row <= 0:
+        if row < 0:
             return
         
         # Deseleziona tutto prima di una nuova selezione, ciclo che evita l'accumulo di selezioni
-        for i in range(1, self.value_table.rows):
+        for i in range(self.value_table.rows):
             self.value_table.deselect_row(i)
 
         self.selected_row_data = None
@@ -144,7 +150,6 @@ class App(ctk.CTk):
 
     # Funzione per formattare i dati del database in liste di liste per la CTkTable
     def format_data(self, rows):
-        header = ["ID", "Componente", "Problema", "Soluzione", "Documento"]
         formatted_data = []
 
         for row in rows:
@@ -154,24 +159,37 @@ class App(ctk.CTk):
                                   row['Soluzione'], 
                                   row['Documentazione']])
         
-        return [header] + formatted_data
+        return formatted_data
 
     # Funzione per caricare i dati del database dentro alla tabella
     def load_data(self):
-        raw_rows = query.get_database()
-        values = self.format_data(raw_rows)
+        raw_rows = query.get_database()                 # lista di dizionari (coppie key-value)
+        values = self.format_data(raw_rows)             # lista di liste (solo valori)
 
         self.value_table.values = values
         self.value_table.update_values(values)
 
-        try:
-            self.value_table.edit_row(0, font=("Roboto", 18, "bold"))
-        except:
-            print("Errore nel font dell'header, applico stile base")
-            self.value_table.edit_row(0)
-
         self.selected_row_data = None
-    
+
+    # Funzione per leggere il contenuto dei Textbox (frame di sinistra)
+    def insert_record(self):
+        self.load_data()
+        raw_row = query.get_max_id()
+        current_id = raw_row[0]["ID"]
+
+        id = int(current_id) + 1
+        component = self.entry_component.get().strip()
+        description = self.text_description.get("1.0", "end-1c").strip()
+        solution = self.text_solution.get("1.0", "end-1c").strip()
+        document = "No"
+
+        query.insert_record(id, component, description, solution, document)
+        self.load_data()
+
+        self.entry_component.delete("0", "end")
+        self.text_description.delete("0.0", "end")
+        self.text_solution.delete("0.0", "end")
+        
 
 # Classe della finestra di dettaglio
 class DetailWindow(ctk.CTkToplevel):
@@ -182,6 +200,7 @@ class DetailWindow(ctk.CTkToplevel):
 
         self.title(f"Dettaglio problema # {self.id}")
         self.center_win_detail(500, 450)
+        self.resizable(False, False)
 
         # Configurazione griglia principale
         self.grid_columnconfigure(0, weight=1)
@@ -190,33 +209,40 @@ class DetailWindow(ctk.CTkToplevel):
         # ======================= FRAME =======================
         self.win_frame = ctk.CTkFrame(self, corner_radius=4)
         self.win_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
-        self.win_frame.grid_columnconfigure(0, weight=1)
+        self.win_frame.grid_columnconfigure((0, 1), weight=1)
         self.win_frame.grid_rowconfigure((1, 3), weight=1)
 
         # Descrizione dettagliata del problema
+        color = "#DBDBDB" if mode == 'Light'else "#2B2B2B"
         self.label_win_description = ctk.CTkLabel(master=self.win_frame, text="Descrizione del problema", font=("Roboto", 18, "bold"))
-        self.label_win_description.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nw")
-        self.text_win_description_detail = ctk.CTkTextbox(master=self.win_frame, font=("Roboto", 15), fg_color="#2B2B2B")
-        self.text_win_description_detail.grid(row=1, column=0, padx=0, pady=(0, 10), sticky="nsew")
+        self.label_win_description.grid(row=0, column=0, columnspan=2, padx=10, pady=(10, 0), sticky="nw")
+        self.text_win_description_detail = ctk.CTkTextbox(master=self.win_frame, font=("Roboto", 15), fg_color=color)
+        self.text_win_description_detail.grid(row=1, column=0, columnspan=2, padx=0, pady=(0, 10), sticky="nsew")
         self.text_win_description_detail.insert("0.0", self.problem)
         self.text_win_description_detail.configure(state="disabled")
 
         # Descrizione dettagliata della soluzione
         self.label_win_solution = ctk.CTkLabel(master=self.win_frame, text="Descrizione della soluzione", font=("Roboto", 18, "bold"))
-        self.label_win_solution.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="nw")
-        self.text_win_solution_detail = ctk.CTkTextbox(master=self.win_frame, font=("Roboto", 15), fg_color="#2B2B2B")
-        self.text_win_solution_detail.grid(row=3, column=0, padx=0, pady=(0, 10), sticky="nsew")
+        self.label_win_solution.grid(row=2, column=0, columnspan=2, padx=10, pady=(10, 0), sticky="nw")
+        self.text_win_solution_detail = ctk.CTkTextbox(master=self.win_frame, font=("Roboto", 15), fg_color=color)
+        self.text_win_solution_detail.grid(row=3, column=0, columnspan=2, padx=0, pady=(0, 10), sticky="nsew")
         self.text_win_solution_detail.insert("0.0", self.solution)
         self.text_win_description_detail.configure(state="disabled")
 
         # Pulsante visualizza documento e chiudi finestra
         self.button_view_doc = ctk.CTkButton(master=self.win_frame, text="Visualizza documento 📄", font=("Roboto", 15))
         self.button_view_doc.grid(row=4, column=0, padx=10, pady=10, sticky="new")
+        self.button_add_doc = ctk.CTkButton(master=self.win_frame, text="Aggiungi documento 🆕", font=("Roboto", 15), command=self.add_document)
+        self.button_add_doc.grid(row=4, column=1, padx=10, pady=10, sticky="new")
         self.button_close_win = ctk.CTkButton(master=self.win_frame, text="Chiudi la finestra ❌", font=("Roboto", 15), fg_color="red", hover_color="#C82333", command=self.destroy)
-        self.button_close_win.grid(row=5, column=0, padx=10, pady=10, sticky="sew")
+        self.button_close_win.grid(row=5, column=0, columnspan=2, padx=10, pady=10, sticky="sew")
 
         # Necessario per portare la finestra in primo piano
         self.after(100, self.lift)
+
+    # Funzione per aggiungere un documento
+    def add_document(self):
+        DocumentExistAllert(self)
 
     # Funzione per centrare la finestra di dettaglio all'apertura
     def center_win_detail(self, width, height):
@@ -275,6 +301,47 @@ class CancelConfirm(ctk.CTkToplevel):
         y = int(((screen_height / 2) - (height / 2)) * scale)
         self.geometry(f"{width}x{height}+{x}+{y}")
 
+
+# Classe per la finestra di conferma cancellazione record
+class DocumentExistAllert(ctk.CTkToplevel):
+    def __init__(self, master):
+        super().__init__(master)
+        
+        self.title("Documento già esistente")
+        self.center_document_exist_win(450, 250)
+        self.resizable(False, False)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        # ======================= FRAME =======================
+        self.document_exist_win_frame = ctk.CTkFrame(self, corner_radius=4)
+        self.document_exist_win_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+        self.document_exist_win_frame.grid_columnconfigure(0, weight=1)
+        self.document_exist_win_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
+
+        self.label_document_exist = ctk.CTkLabel(master=self.document_exist_win_frame, text="Per questo record è già presente un documento.\nVuoi sostituirlo?", font=("Roboto", 17, "bold"))
+        self.label_document_exist.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+        self.button_view_document = ctk.CTkButton(master=self.document_exist_win_frame, text="Visualizza documento 📄", font=("Roboto", 15))
+        self.button_view_document.grid(row=1, column=0, padx=10, pady=10, sticky="sew")
+        self.button_replace_document = ctk.CTkButton(master=self.document_exist_win_frame, text="Sostituisci documento ↩️", font=("Roboto", 15))
+        self.button_replace_document.grid(row=2, column=0, padx=10, pady=10, sticky="ew")
+        self.button_close_win_document = ctk.CTkButton(master=self.document_exist_win_frame, text="Chiudi la finestra ❌", font=("Roboto", 15), fg_color="red", hover_color="#C82333", command=self.destroy)
+        self.button_close_win_document.grid(row=3, column=0, columnspan=2, padx=10, pady=10, sticky="new")
+
+        self.attributes("-topmost", True)
+        self.after(10, self.grab_set)
+        self.after(10, self.focus_force)
+
+    def center_document_exist_win(self, width, height):
+        self.update_idletasks()
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        scale = self._get_window_scaling()
+
+        x = int(((screen_width / 2) - (width / 2)) * scale)
+        y = int(((screen_height / 2) - (height / 2)) * scale)
+        self.geometry(f"{width}x{height}+{x}+{y}")
 
 
 if __name__ == "__main__":
