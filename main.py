bc09a5ae663fa6bfad47220103328f03e4993bd7
merola.riccardo <merola.riccardo02@gmail.com>
2026-05-29 10:19:38 +0200

    Logica per sostituzione allegato su record che ne ha già uno

--- main.py
+++ main.py
@@ -267,7 +267,7 @@ class App(ctk.CTk):
             # Messaggio che avvisa di file con stesso nome già presente
             msg_exist = CTkMessagebox(
                 title="File esistente", 
-                message=f'Il file "{filename}" esiste già.\nVuoi sovrascriverlo?', 
+                message=f'Esiste già un file "{filename}".\nVuoi sovrascriverlo?', 
                 icon="warning", 
                 option_1="Si", 
                 option_2="No",
@@ -477,8 +477,11 @@ class CancelConfirm(ctk.CTkToplevel):
 
 # Classe per la finestra di conferma cancellazione record
 class DocumentExistAllert(ctk.CTkToplevel):
-    def __init__(self, master):
+    def __init__(self, master, data):
         super().__init__(master)
+
+        self.id, self.component, self.description, self.solution, self.document, self.root = data
+        self.master = master
 
         self.title("Documento già esistente")
         self.center_document_exist_win(450, 250)
@@ -497,7 +500,7 @@ class DocumentExistAllert(ctk.CTkToplevel):
         self.label_document_exist.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
         #self.button_view_document = ctk.CTkButton(master=self.document_exist_win_frame, text="Visualizza documento 📄", font=("Roboto", 15))
         #self.button_view_document.grid(row=1, column=0, padx=10, pady=10, sticky="sew")
-        self.button_replace_document = ctk.CTkButton(master=self.document_exist_win_frame, text="Sostituisci documento ↩️", font=("Roboto", 15))
+        self.button_replace_document = ctk.CTkButton(master=self.document_exist_win_frame, text="Sostituisci documento ↩️", font=("Roboto", 15), command=self.change_document)
         self.button_replace_document.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
         self.button_close_win_document = ctk.CTkButton(master=self.document_exist_win_frame, text="Chiudi la finestra ❌", font=("Roboto", 15), fg_color="red", hover_color="#C82333", command=self.destroy)
         self.button_close_win_document.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="new")
@@ -516,7 +519,60 @@ class DocumentExistAllert(ctk.CTkToplevel):
         x = int(((screen_width / 2) - (width / 2)) * scale)
         y = int(((screen_height / 2) - (height / 2)) * scale)
         self.geometry(f"{width}x{height}+{x}+{y}")
+    
+    # Funzione pre sostituire il documento già presente
+    def change_document(self):
+        self.master.destroy()
+        self.destroy()
+        data = query.get_dettaglio(self.id)
+        print(data)
 
+        msg_warning = CTkMessagebox(
+            title="ATTENZIONE",
+            message="Questa oprazione elimina il file attuale\nVuoi sostituire il file attuale?",
+            icon="warning",
+            option_1="Si",
+            option_2="No",
+            justify="center"
+        )
+
+        if msg_warning.get() == "No":
+            print("Non voglio cambiare il file")
+            self.destroy
+            return
+        
+        selected_file = filedialog.askopenfilename(title="Seleziona un file", filetypes=[("Turi i file", "*.*")])
+        filename = os.path.basename(selected_file)
+        destination_path = os.path.join(documents_root, filename)
+
+        if not os.path.isfile(destination_path):
+            print("Nuovo file")
+            shutil.copy(selected_file, documents_root)
+            self.root = destination_path
+            query.edit_record(self.id, self.component, self.description, self.solution, self.document, self.root)
+            self.master.master.load_data()
+            return
+        
+        msg_exist = CTkMessagebox(
+            title="File esistete",
+            message=f'Esiste già un file "{filename}".\nVuoi sovrascriverlo?',
+            icon="warning",
+            option_1="Si", 
+            option_2="No",
+            justify="center"
+        )
+
+        if msg_exist.get() == "No":
+            print("Non voglio sovrascrivere")
+            self.destroy
+        else:
+            shutil.copy(selected_file, destination_path)
+            print("File sovrascritto")
+            self.root = destination_path
+            query.edit_record(self.id, self.component, self.description, self.solution, self.document, self.root)
+            self.master.master.load_data()
+            return
+        
 
 class DocumentNotExistAllert(ctk.CTkToplevel):
     def __init__(self, master, data):
@@ -582,7 +638,7 @@ class DocumentNotExistAllert(ctk.CTkToplevel):
         
         msg_exist = CTkMessagebox(
             title="File esistente",
-            message=f'Il file "{filename}" esiste già.\nVuoi sovrascriverlo?',
+            message=f'Esiste già un file "{filename}".\nVuoi sovrascriverlo?',
             icon="warning",
             option_1="Si",
             option_2="No",
